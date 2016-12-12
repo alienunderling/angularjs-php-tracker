49f99b3a3abf608269427de374d54bffc1e6e171
alienunderling@hotmail.com <alienunderling@hotmail.com>
2016-12-11 23:52:32 -0700

    Stubbed out the routes

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,4 +1,21 @@
 // ROUTES
 tracker.config(['$routeProvider', function ($routeProvider) {
-    
+    $routeProvider    
+    .when('/', {
+        templateUrl: '',
+        controller: ''//'homePageController'
+    })
+    .when('/addService', {
+        templateUrl: '',
+        controller: ''//'addServiceController'
+    })    
+    .when('/viewAppointments', {
+        templateUrl: '',
+        controller: ''//'appointmentsController'
+    })    
+    .when('/serviceAdded', {
+        templateUrl: '',
+        controller: ''//'serviceAddedController'
+    })    
+    .otherwise({ redirectTo: '/' });
 }]);
